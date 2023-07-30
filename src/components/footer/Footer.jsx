d41dc1436e749e117dd3b1ecfbb7f5a0db859cd3
4nth0ny1 <acatullo4@gmail.com>
2023-07-29 22:13:16 -0500

    added navbar and footer basic functions'

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex flex-row justify-between">
+      <div>©2023 Lamamia. All rights reserved.</div>
+      <div className="flex flex-row gap-4">
+        <Image
+          src="/1.png"
+          width={25}
+          height={25}
+          alt="Lama Dev Facebook Account"
+        />
+        <Image src="/2.png" width={25} height={25} alt="Lama Dev" />
+        <Image src="/3.png" width={25} height={25} alt="Lama Dev" />
+        <Image src="/4.png" width={25} height={25} alt="Lama Dev" />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import Link from "next/link";
+
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex flex-row justify-between p-4">
+      <Link href="/">Lama</Link>
+      <div>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className="px-2">
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
